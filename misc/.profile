7b5c97f03a58b83aadbee2535a05bf98863ba93d
Grzegorz Balcerek <gbalcerek72@gmail.com>
2025-10-10 11:30:41 +0200

    misc

---
+++ misc/.profile
@@ -0,0 +1,24 @@
+# cp .profile $HOME/.profile
+# cp $HOME/.conifg/misc/.profile $HOME/.profile
+
+export EDITOR=vim
+export OPENGNT_BASE_TEXT="$HOME/gh/OpenGNT/OpenGNT_version3_3.csv"
+export PATH=".:"$PATH:"/c/helm:/c/Program Files/Mozilla Firefox:/c/Program Files/LibreOffice/program:/c/Program Files/Emacs/emacs-29.1/bin"
+export PYTHONIOENCODING=utf8
+export VIMINIT="source $HOME/.config/vim/vimrc"
+
+alias cdd="cd ~/doc"
+alias cdD="cd ~/Downloads"
+alias cdde="cd ~/doc/emacs"
+alias cddr="cd ~/doc/rbs"
+alias cddrc="cd ~/doc/rbs/cov26"
+alias cdg="cd ~/gh"
+alias cdh="cd $HOME"
+alias cdl="cd ~/lib"
+alias cdr="cd ~/rep"
+alias cdrr="cd ~/rep/rbs"
+alias cdrra="cd ~/rep/rbs/app"
+alias cdrro="cd ~/rep/rbs/ognt"
+
+alias g=git
+
---
+++ misc/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,18 @@
+# copy Microsoft.PowerShell_profile.ps1 $PROFILE
+# copy $HOME\.config\misc\Microsoft.PowerShell_profile.ps1 $PROFILE
+
+function cdd {Set-Location -Path $HOME\doc}
+function cdD {Set-Location -Path $HOME\Downloads}
+function cdde {Set-Location -Path $HOME\doc\emacs}
+function cddr {Set-Location -Path $HOME\doc\rbs}
+function cddrc {Set-Location -Path $HOME\doc\rbs\cov26}
+function cdg {Set-Location -Path $HOME\gh}
+function cdh {Set-Location -Path $HOME}
+function cdl {Set-Location -Path $HOME\lib}
+function cdr {Set-Location -Path $HOME\rep}
+function cdrr {Set-Location -Path $HOME\rep\rbs}
+function cdrra {Set-Location -Path $HOME\rep\rbs\app}
+function cdrro {Set-Location -Path $HOME\rep\rbs\ognt}
+
+Set-Alias -name g -value git
+
