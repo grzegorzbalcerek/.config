2d440089fe6f39a65cc524e8b5387c1f3f33a12c
Grzegorz Balcerek <gbalcerek72@gmail.com>
2025-10-21 17:55:22 +0200

    .\ognt.lua

--- nvim/plugin/ognt.lua
+++ nvim/plugin/ognt.lua
@@ -1,58 +1,64 @@
 
-function load_file(file)
+function load_book(book)
+    if book then
+        vim.g.OgntBook = book
+    elseif not vim.g.OgntBook then
+        error("Book number not given. Set vim.g.OgntBook or provide the book number as argument")
+    end
+    local file = "book" .. tostring(vim.g.OgntBook) .. ".lua"
     local words = {}
     local concordance = {}
-    local nw = 1
+    local word_count = 0
+    local concordance_count = 0
     function Word(w)
-        words[nw] = w
-        nw = nw + 1
+        word_count = word_count + 1
+        words[word_count] = w
     end
     function Concordance(c)
+        concordance_count = concordance_count + 1
         concordance[c.sn] = c
     end
     dofile(file)
-    vim.g.RbsFileWords = words
-    vim.g.RbsWords = words
-    vim.g.RbsConcordance = concordance
+    vim.g.OgntBookWords = words
+    vim.g.OgntRangeWords = words
+    vim.g.OgntBookConcordance = concordance
+    return "Loaded " .. tostring(word_count) .. " words and " .. tostring(concordance_count) .. " concordance entries from file " .. file .. "."
 end
 
-vim.api.nvim_create_user_command("RbsLoad",
-    function(opts)
-        local file = opts.fargs[1]
-        load_file(file)
-      end,
-      { nargs = 1 })
+vim.api.nvim_create_user_command("OgntLoadBook", function(opts) print(load_book(opts.fargs[1])) end, { nargs = "?" })
 
-function sn_info(sn)
-    return vim.g.RbsConcordance[sn].sn .. " " ..
-           vim.g.RbsConcordance[sn].grlexeme .. " " ..
-           vim.g.RbsConcordance[sn].enlexeme .. " " ..
-           vim.g.RbsConcordance[sn].t1lexeme .. " " ..
-           vim.g.RbsConcordance[sn].t2lexeme .. " " ..
-           vim.g.RbsConcordance[sn].count
+function _sn_info(entry)
+    return entry.sn .. " " ..
+           entry.grlexeme .. " " ..
+           entry.t1lexeme .. " " ..
+           entry.t2lexeme .. " " ..
+           entry.enlexeme .. " " ..
+           entry.count .. " " ..
+           entry.addrs
 end
 
-vim.api.nvim_create_user_command("RbsSn",
-    function(opts)
-        local sn = opts.fargs[1]
-        print(sn_info(sn))
-        print(vim.g.RbsConcordance[sn].addrs)
-        print(vim.g.RbsConcordance[sn].verses)
-    end,
-    { nargs = 1 })
+function concordance_sn(sn)
+    local entry = vim.g.OgntBookConcordance[sn]
+    if entry then
+        print(_sn_info(entry))
+    else
+        error("Strong number " .. sn .. " entry not found")
+    end
+end
 
-vim.api.nvim_create_user_command("RbsCFind",
-    function(opts)
-        local str = opts.fargs[1]
-        for k,v in pairs(vim.g.RbsConcordance) do
-            if string.match(v.sn, str) or string.match(v.enlexeme, str) or string.match(v.t2lexeme, str) then
-                print(sn_info(v.sn))
-            end
+vim.api.nvim_create_user_command("ConcordanceSn", function(opts) concordance_sn(opts.fargs[1]) end, { nargs = 1 })
+
+function concordance_find(str)
+    for k,v in pairs(vim.g.OgntBookConcordance) do
+        if string.match(v.sn, str) or string.match(v.enlexeme, str) or string.match(v.t2lexeme, str) then
+            print(_sn_info(v))
         end
-    end,
-    { nargs = 1 })
+    end
+end
 
-function range(from, to)
+vim.api.nvim_create_user_command("ConcordanceFind", function(opts) concordance_find(opts.fargs[1]) end, { nargs = 1 })
+
+function range_words(from, to)
     local data = {}
     local n = 1
     local field_from = "verse"
@@ -64,7 +70,7 @@ function range(from, to)
         field_to = "addr"
     end
     local phase = 1 -- 1: before the first verse, 2: in range, 3: in the last verse
-    for k,v in pairs(vim.g.RbsWords) do
+    for k,v in pairs(vim.g.OgntBookWords) do
         if phase == 1 then
             if v[field_from] == from then
                 phase = 2
@@ -81,30 +87,28 @@ function range(from, to)
             break
         end
     end
-    vim.g.RbsWords = data
+    vim.g.OgntRangeWords = data
     print("Lines " .. tostring(n))
 end
 
-vim.api.nvim_create_user_command("RbsRange",
-    function(opts)
-        local from = opts.fargs[1]
-        local to = opts.fargs[2]
-        if from and to then
-            range(from, to)
-        else
-            vim.g.RbsWords = vim.g.RbsFileWords
-            print("Range unset")
-        end
-    end,
-    { nargs = "*" })
+function range(from, to)
+    if from and to then
+        range_words(from, to)
+    else
+        vim.g.OgntRangeWords = vim.g.OgntBookWords
+        print("Range unset")
+    end
+end
 
-function count_sn(sn)
+vim.api.nvim_create_user_command("Range", function(opts) range(opts.fargs[1], opts.fargs[2]) end, { nargs = "*" })
+
+function range_count_sn(sn)
 
     local verses = {}
     local list = ""
     local count = 0
 
-    for k,v in pairs(vim.g.RbsWords) do
+    for k,v in pairs(vim.g.OgntRangeWords) do
         if v.sn == sn then
           count = count + 1
           if not verses[v.verse] then
@@ -117,51 +121,15 @@ function count_sn(sn)
         end
     end
 
-    local result = vim.g.RbsConcordance[sn]
-    return "The word “" .. enlexeme .. "” (" .. t1lexeme .. ") is repeated " .. count .. " times (" .. list .. ")."
+    local entry = vim.g.OgntBookConcordance[sn]
+    return "The word “" .. entry.enlexeme .. "” (" .. entry.t1lexeme .. ") is repeated " .. count .. " times (" .. list .. ")."
 end
 
-function repeated_sn(sn)
+vim.api.nvim_create_user_command("RangeCountSnPrint", function(opts) print(range_count_sn(opts.fargs[1])) end, { nargs = 1 })
 
-    local verses = {}
-    local list = ""
-    local count = 0
-    local enlexeme = ""
-    local t1lexeme = ""
+vim.api.nvim_create_user_command("RangeCountSnPaste", function(opts) vim.api.nvim_paste(range_count_sn(opts.fargs[1]), true, -1) end, { nargs = 1 })
 
-    for k,v in pairs(vim.g.RbsWords) do
-        if v.sn == sn then
-          count = count + 1
-          enlexeme = v.enlexeme
-          t1lexeme = v.t1lexeme
-          if not verses[v.verse] then
-              verses[v.verse] = true
-              if not (list == "") then
-                  list = list .. ", "
-              end
-              list = list .. v.verse
-          end
-        end
-    end
-
-    return "The word “" .. enlexeme .. "” (" .. t1lexeme .. ") is repeated " .. count .. " times (" .. list .. ")."
-end
-
-vim.api.nvim_create_user_command("RbsInsertRepeatedSn",
-    function(opts)
-        local description = repeated_sn(opts.fargs[1])
-        vim.api.nvim_paste(description, true, -1)
-    end,
-    { nargs = 1 })
-
-vim.api.nvim_create_user_command("RbsPrintRepeatedSn",
-    function(opts)
-        local description = repeated_sn(opts.fargs[1])
-        print(description)
-    end,
-    { nargs = 1 })
-
-if vim.g.rbs_file then
-    load_file(vim.g.rbs_file)
+if vim.g.OgntBook then
+    load_book()
 end
 
